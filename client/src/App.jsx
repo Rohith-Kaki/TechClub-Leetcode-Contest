f200273ba3a0688d21bfdf800a3af48f78f972ed
Rohith-Kaki <rohithlahari55@gmail.com>
2025-11-24 03:07:40 +0530

    Add forgor password routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 import HomePage from "./pages/home.jsx";
@@ -8,21 +8,27 @@ import ProgressPage from "./pages/problems.jsx";
 import RulesPage from "./pages/rules.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import UpdatePasswordPage from "./pages/UpdatePasswordPage";
 
 export default function App() {
   return (
     <AuthProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/rules" element={<RulesPage />} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rules" element={<RulesPage />} />
 
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-          <Route path="/leaderboard" element={<LeaderBoard />} />
-          <Route path="/problems" element={<ProgressPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path="/problems" element={<ProgressPage />} />
+
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/update-password" element={<UpdatePasswordPage />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </AuthProvider>
   );
 }
