c6951cf1d26e2e57f39f6d77831dae81087c9ba2
Rohith-Kaki <rohithlahari55@gmail.com>
2025-11-28 02:42:00 +0530

    Add CORS origin

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(
   cors({
     origin: [
       "https://leetcode-contest.techclub.cloud",
+      "https://techclub.cloud",
+      "http://techclub.cloud",
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "http://localhost:5173",
